b0e8f89e433bfa612d5b1a389ca1e8e667788ad6
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-05-07 20:05:44 +0900

    수정

--- test1.py
+++ test1.py
@@ -3,3 +3,17 @@ import streamlit as st
 st.title('title')
 st.header('this is header')
 st.subheader('this is subheader')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
