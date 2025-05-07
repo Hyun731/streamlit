1bd2f8424ea23f3d541905c40cf77b711a956b58
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-05-07 20:20:20 +0900

    ss

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 col1,col2 = st.columns([2,3])
-tab1,tab2 = st.tabs(['Tab B', 'Tab A'])
+tab1,tab2 = st.tabs(['Tab A', 'Tab B'])
 with col1:
     st.title('here is column1 title')
     with tab2 :
